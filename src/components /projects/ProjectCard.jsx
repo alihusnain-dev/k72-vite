7cf3d45fbbf013311a62927e8e98d57654036648
Muhammad Ali Husnain/ <alihusnainmughal215@gmail.com>
2026-03-04 15:40:01 +0500

    add the projects page  component

--- src/components /projects/ProjectCard.jsx
+++ src/components /projects/ProjectCard.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
-const ProjectCard = (img) => {
+gsap.registerPlugin(ScrollTrigger)
+
+const ProjectCard = (props) => {
+    const boxRef = useRef(null)
+
+    useGSAP(() => {
+        gsap.from(boxRef.current, {
+            height: "350px",
+            ease: "none",
+            scrollTrigger: {
+                trigger: boxRef.current,
+                start: "top 90%",
+                end: "top 40%",
+                scrub: 2,
+            }
+        })
+    }, { scope: boxRef })
+
     return (
-        <div className="box bg-amber-700 w-full h-[750px] overflow-hidden hover:rounded-4xl transition-all duration-300 ease-in-out cursor-pointer ease">
-            <img src={img} alt="" className='w-full h-full object-cover' />
-
+        <div
+            ref={boxRef}
+            className="box bg-amber-700 w-full h-[750px] overflow-hidden hover:rounded-[2vw] transition-all duration-300 ease-in-out cursor-pointer"
+        >
+            <img src={props.img} alt="" className='w-full h-full object-cover' />
         </div>
     )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,30 +1,17 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
+
 import React from 'react'
+import ProjectCard from '../components /projects/ProjectCard'
 
 const Projects = () => {
     const projectImages = [
         "/caseStudies/chalaxeur.jpg",
         "/caseStudies/Fruite-bbq.jpg",
-        "/caseStudies/iA_brand.jpg",
+        "/caseStudies/iA_brand.png",
         "/caseStudies/opto.jpg",
         "/caseStudies/PJC_SiteK72.jpg",
     ]
 
-    useGSAP(() => {
-        gsap.registerPlugin(ScrollTrigger)
-        gsap.from(".box", {
-            height: "350px",
-            ease: "none",
-            scrollTrigger: {
-                trigger: ".box",
-                start: "top 70%",
-                end: "bottom 70%",
-                scrub: 2,
-            }
-        })
-    })
+
     return (
         <div className='p-4'>
             <div className="mt-[40vh] flex">
@@ -32,14 +19,9 @@ const Projects = () => {
                 <h1 className='text-[10vw] font-medium uppercase'>Projects</h1> <p className='pt-[6%] pl-[1%] md:text-2xl'>{projectImages.length}</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full min-h-screen -mt-10">
-                <div className="box bg-amber-700 w-full h-[750px] overflow-hidden hover:rounded-4xl transition-all duration-300 ease-in-out cursor-pointer ease">
-                    <img src="/caseStudies/chalaxeur.jpg" alt="" className='w-full h-full object-cover' />
-
-                </div>
-                <div className="box bg-amber-700 w-full h-[750px] overflow-hidden hover:rounded-4xl transition-all duration-300 ease-in-out cursor-pointer ease">
-                    <img src="/caseStudies/Fruite-bbq.jpg" alt="" className='w-full h-full object-cover' />
-
-                </div>
+                {projectImages.map((img, index) => (
+                    <ProjectCard key={index} img={img} />
+                ))}
 
             </div>
         </div>
